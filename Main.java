d62ade6cc9c5d10ad05460be900615bd5d48eb07
lucas-po <lpokrywk@mail.umw.edu>
2019-11-24 21:33:03 +0000

    Formatting stuff 	modified:   Board.java 	modified:   Main.java

--- Main.java
+++ Main.java
@@ -6,27 +6,36 @@ public class Main {
 		Scanner input = new Scanner(System.in);
 		Board board = Board.getInstance();
 
-		System.out.println("All events you see in this game are real. They simply take place a long time ago, in a " +
-				"galaxy far, far away...");
-		System.out.println();
-		System.out.println("The multidimensional barrier has been torn open, throwing a multitude of beings and items into our world.");
-		System.out.println("Unfortunately, through a sheer statistical anomaly, all but one of these beings are evil!");
-		System.out.println("Our hero, torn from their own world and/or time, now fights to rid our world of evil before ");
-		System.out.println("returning to their own world (which will happen offscreen).");
-		System.out.println("Can you help guide them toward where they need to be?");
+		String fileName = "";
+		char choice;
 
-		System.out.println();
+		System.out.println("Would you like to start from a previous save (Y or N)? ");
+		choice = input.next().charAt(0);
+		if((choice == 'N') | (choice == 'n')){
+			System.out.println("All events you see in this game are real. They simply take place a long time ago, in a " +
+					"galaxy far, far away...");
+			System.out.println();
+			System.out.println("The multidimensional barrier has been torn open, throwing a multitude of beings and items into our world.");
+			System.out.println("Unfortunately, through a sheer statistical anomaly, all but one of these beings are evil!");
+			System.out.println("Our hero, torn from their own world and/or time, now fights to rid our world of evil before ");
+			System.out.println("returning to their own world (which will happen offscreen).");
+			System.out.println("Can you help guide them toward where they need to be?");
 
-		System.out.println("Your goal: Defeat all of the enemies on the board (denoted by &).");
-		System.out.println("At the beginning of each battle, you may choose which armor and weapon to use during the fight.");
-		System.out.println();
+			System.out.println();
 
-		//Generate a new board and print it to the screen
-		System.out.printf("Welcome, %s! %n", Character.player().getName());
+			System.out.println("Your goal: Defeat all of the enemies on the board (denoted by &).");
+			System.out.println("At the beginning of each battle, you may choose which armor and weapon to use during the fight.");
+			System.out.println();
 
-		System.out.println("Below is list of symbols and actions that you can make with your character.");
-		System.out.println("To begin, please choose an action from the menu: ");
-		System.out.println("(You must press enter between every action)");
+			//Generate a new board and print it to the screen
+			System.out.printf("Welcome, %s! %n", Character.player().getName());
+
+			System.out.println("Below is list of symbols and actions that you can make with your character.");
+			System.out.println("To begin, please choose an action from the menu: ");
+			System.out.println("(You must press enter between every action)");
+		}/*if((choice == 'Y') | (choice == 'y')){
+			board.Board(fileName);
+		}*/
 
 		printMenu();
 		/*
@@ -46,7 +55,7 @@ public class Main {
 				play = input.next().charAt(0);
 				board.play(play);
 
-			} while (play != 'Q');
+			} while ((play != 'Q') | (play != 'q'));
 		}
 	}
 

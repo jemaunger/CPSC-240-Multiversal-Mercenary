dd893220542ab3176b6ddf28bb18b5096c507a6b
jemaunger <jemaunger@gmail.com>
2019-12-05 19:41:51 +0000

    modified:   FoodGenerator.class 	modified:   FoodGenerator.java

--- FoodGenerator.java
+++ FoodGenerator.java
@@ -6,20 +6,20 @@ public class FoodGenerator {
     ArrayList<Food> foodList = new ArrayList<Food>();
 //creates and stores the different types of food that can be spread throughout the board.
     FoodGenerator() {
-        foodList.add(new Food("Beets", 10));
+        foodList.add(new Food("Beets", 5));
         foodList.add(new Food("Potato", 10));
         foodList.add(new Food("Slice of cake", 10));
-        foodList.add(new Food("Cookie", 20));
+        foodList.add(new Food("Cookie", 10));
         foodList.add(new Food("Watermelon Slice",20));
         foodList.add(new Food("Chocolate Frog", 20));
-        foodList.add(new Food("Carrot", 30));
-        foodList.add(new Food("Raw Steak",30));
-        foodList.add(new Food("Apple",30));
+        foodList.add(new Food("Carrot", 5));
+        foodList.add(new Food("Raw Steak",20));
+        foodList.add(new Food("Apple",20));
         foodList.add(new Food("Baguette", 50));
-        foodList.add(new Food("Suspicious Stew",60));
-        foodList.add(new Food("Pumpkin Pie", 80));
-        foodList.add(new Food("Steak", 80));
-        foodList.add(new Food("Whole cake",140));
+        foodList.add(new Food("Suspicious Stew",30));
+        foodList.add(new Food("Pumpkin Pie", 40));
+        foodList.add(new Food("Steak", 40));
+        foodList.add(new Food("Whole cake",100));
 
     }
 //randomly generates food and spreads it out.
